d2050bea42d58fae368eac190264110ded74dea5
abdulkadirmusuk <58270978+abdulkadirmusuk@users.noreply.github.com>
2021-08-21 17:28:38 +0300

    [AKM] Selling project event bus base implementation

--- src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
@@ -6,14 +6,10 @@ namespace EventBus.Base.Abstraction
     //AzureServiceBus ve RabbitMQ bu interface i kullanarak işlemlerini gerçekleştirecek 
     public interface IEventBus
     {
-        void Publis(IntegrationEvent @event);//Service dışarıya bir event fırlatır
-
+        void Publish(IntegrationEvent @event);//Service dışarıya bir event fırlatır
         //Subscribe olacak event ve o event i kimin handle edeceğini söyledik (Örn: OrderIntegrationCreatedEvent subsribtion verilecek ve handler türü verilecek(rabbitmq-asb) ve Handle methodu tetiklenecek)
+        
         void Subscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
         void UnSubscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
-
-
-
-
     }
 }
--- src/BuildingBlocks/EventBus/EventBus.Base/Event/BaseEventBus.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Event/BaseEventBus.cs
@@ -1,4 +1,7 @@
 ﻿using EventBus.Base.Abstraction;
+using EventBus.Base.SubManagers;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,38 +21,68 @@ namespace EventBus.Base.Event
         {
             eventBusConfig = config;
             ServiceProvider = serviceProvider;
-            SubsManager = new InMemoryEventBusSubscriptionsManager(ProcessEventName);
+            SubsManager = new InMemoryEventBusSubscriptionManager(ProcessEventName); //Subscription inmemory olarak set edilmiş. Başka bir tür geldiğinde bu değişecektir.
         }
 
         public virtual string ProcessEventName(string eventName)
         {
+            //event name verirken başından veya sonundan trim etmek için kullanılrı
             if (eventBusConfig.DeleteEventPrefix)
                 eventName = eventName.TrimStart(eventBusConfig.EventNamePrefix.ToArray());
 
             if (eventBusConfig.DeleteEventSuffix)
-                eventName = eventName.TrimStart(eventBusConfig.DeleteEventSuffix.ToArray());
+                eventName = eventName.TrimStart(eventBusConfig.EventNameSuffix.ToArray());
 
             return eventName;
         }
 
-
-        public void Publis(IntegrationEvent @event)
+        public virtual string GetSubName(string eventName)
         {
-            throw new NotImplementedException();
+            //Örn : NotificationService.OrderCreatedIntegrationEvent şeklinde bir subscription name döner
+            //Örn : NotificationService.OrderCreated da olabilir suffix den kırpılacaksa. Bu bizim queue name olacak
+            return $"{eventBusConfig.SubscriberClientAppName}.{ProcessEventName(eventName)}";
         }
 
-        public void Subscribe<T, TH>()
-            where T : IntegrationEvent
-            where TH : IIntegrationEventHandler<T>
+        public virtual void Dispose()
         {
-            throw new NotImplementedException();
+            eventBusConfig = null;
         }
 
-        public void UnSubscribe<T, TH>()
-            where T : IntegrationEvent
-            where TH : IIntegrationEventHandler<T>
+        public async Task<bool> ProcessEvent(string eventName, string message)
         {
-            throw new NotImplementedException();
+            //rabbitmq veya asb üzerinden gelen mesajlar bu method a düşecek
+            //event ismi ve message da deserialize edilebilir bir obje olacak
+            eventName = ProcessEventName(eventName);
+
+            var processed = false;
+
+            if (SubsManager.HasSubscriptionsForEvent(eventName)) // Eğer gelen eventName e göre onu dinleyen biri varsa işlem yapılacaktır
+            {
+                var subscriptions = SubsManager.GetHandlersForEvent(eventName); //Gelen event e kimler subscribe onları bulur
+                using (var scope = ServiceProvider.CreateScope())//Burdaki service providerlar aynı scope da türetilmesi için create scope yaptık
+                {
+                    foreach (var subscription in subscriptions)
+                    {
+                        var handler = ServiceProvider.GetService(subscription.HandleType); //Örn : OrderIntegrationEventHandler ı bana ver demektir. 
+                        if (handler == null) continue;
+
+                        var eventType = SubsManager.GetEventTypeByName($"{eventBusConfig.EventNamePrefix}{eventName}{eventBusConfig.EventNameSuffix}");
+                        var integrationEvent = JsonConvert.DeserializeObject(message, eventType);
+                        var concreteType = typeof(IIntegrationEventHandler<>).MakeGenericType(eventType);
+                        await (Task)concreteType.GetMethod("Handle").Invoke(handler, new object[] { integrationEvent }); //Gelen eventin türüne göre dinamik olarak handle methodu çalışır
+                        //message içerisinde integration event i yakalar ve ilgili serviste handle eder
+                    }
+                }
+                processed = true;
+            }
+            return processed;
         }
+
+        //Aşağıdaki methodların impelentasyonları rabbitmq veya asb tarafında olacağında içlerini doldurmadık
+        public abstract void Publish(IntegrationEvent @event);
+
+        public abstract void Subscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+
+        public abstract void UnSubscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
     }
 }
